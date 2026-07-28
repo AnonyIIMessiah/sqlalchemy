822d095ca268f8a8b000872b3487ed23acc26d7e
Jai <jaij.kishore.ic@smartsheet.com>
2026-07-28 17:30:49 +0530

    intialize repo

---
+++ tests/integration/test_create_tables.py
@@ -0,0 +1,53 @@
+import time
+
+from sqlalchemy import Column, Integer, String, create_engine, text
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {"schema": "public"}
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+
+
+def test_creates_tables(clean_primary, clean_secondary):
+    from sqlalchemy_pglogical import replicate_ddl
+
+    primary_engine = create_engine(clean_primary)
+    PrimarySession = sessionmaker(primary_engine)
+    logical_engine = create_engine(clean_secondary)
+    LogicalSession = sessionmaker(logical_engine)
+
+    table_query = text("""
+    SELECT table_schema||'.'||table_name AS full_rel_name 
+    FROM information_schema.tables 
+    WHERE table_schema = 'public'""")
+
+    for Session in (PrimarySession, LogicalSession):
+        # clean the database
+        with Session() as session:
+            session.execute(text("""DROP TABLE IF EXISTS users CASCADE"""))
+            session.commit()
+            session.close()
+        # confirm it's really clean
+        with Session() as session:
+            tables = session.execute(table_query)
+            assert tables.first() is None
+
+    # make the tables
+    Base.metadata.create_all(primary_engine)
+
+    # confirm the tables got made
+    with LogicalSession() as session:
+        for _ in range(10):
+            logical_tables = session.execute(table_query)
+            table = logical_tables.first()
+            if table is not None:
+                break
+            time.sleep(0.5)
+    assert table is not None
---
+++ tests/unit/test_ddl.py
@@ -0,0 +1,25 @@
+import sqlalchemy as sa
+from sqlalchemy.sql.compiler import DDLCompiler
+
+import sqlalchemy_pglogical
+
+
+def test_create_table_is_replicated():
+    bind = sa.create_engine("postgresql://invalid:invalid@invalid/invalid")
+    metadata = sa.MetaData()
+    table = sa.Table("my_table", metadata, sa.Column("my_column", sa.Integer))
+    create = sa.schema.CreateTable(table)
+    compiled = create.compile(bind)
+    assert "create" in compiled.string.lower()
+    assert compiled.string.startswith(
+        "SELECT pglogical.replicate_ddl_command($sqlalchemypglogical$"
+    )
+    assert compiled.string.endswith("$sqlalchemypglogical$)")
+
+
+def test_all_subclasses_recursion():
+    last = type("a", (object,), {})
+    first = last
+    for i in range(100):
+        last = type(f"a{i}", (last,), {})
+    assert len(sqlalchemy_pglogical.replicate_ddl.all_subclasses(first)) == 100
